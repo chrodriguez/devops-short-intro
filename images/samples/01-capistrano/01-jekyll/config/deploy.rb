3116dc3db4a97a82e750d654e25d775b3ed6be4d
Christian Rodriguez <chrodriguez@gmail.com>
2016-10-11 01:16:43 -0300

    ignoro logs de capistrano

--- images/samples/01-capistrano/01-jekyll/config/deploy.rb
+++ images/samples/01-capistrano/01-jekyll/config/deploy.rb
@@ -1,8 +1,6 @@
-lock '3.5.0'
-
 set :application, 'capistrano-sample'
 set :repo_url, 'git@github.com:Mikroways/mikroways.net.git'
-#set :branch, 'demo-capistrano-curso'
+set :branch, 'demo-capistrano-curso'
 set :deploy_to, '/opt/sites/jekyll'
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.3.0'
